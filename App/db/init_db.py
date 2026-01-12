c6cc670efab8d17730f6dbfd13dda56278765758
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-01-12 10:34:50 +0330

    init_db file created.
    with this file we can creat and use database tables.

---
+++ App/db/init_db.py
@@ -0,0 +1,11 @@
+from db.database import engine, Base, SessionLocal
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
