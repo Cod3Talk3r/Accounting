a50828125d9f7de9a446d251d9d3761aa4b64fc7
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-01-15 09:23:35 +0330

    new errors added to errors file.

--- App/errors.py
+++ App/errors.py
@@ -1,7 +1,25 @@
 from fastapi import status, HTTPException
 
 
-class UserNotFound(HTTPException):
+class NotFoundUser(HTTPException):
     def __init__(self):
         self.status_code = status.HTTP_404_NOT_FOUND
         self.detail = "User Not Found!"
+
+
+class WrongUsernameOrPassword(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_404_NOT_FOUND
+        self.detail = "Wrong Username Or Password!"
+
+
+class ExistUsername(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_409_CONFLICT
+        self.detail = "This Username Exists."
+
+
+class ExistEmail(HTTPException):
+    def __init__(self):
+        self.status_code = status.HTTP_409_CONFLICT
+        self.detail = "This Email Exists."
