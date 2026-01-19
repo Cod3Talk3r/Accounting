75e6ac245934d4528df0de83eb1953cc748b9f56
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-01-20 02:19:10 +0330

    register input validating.

--- App/inout/input_.py
+++ App/inout/input_.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class UserRegisterInput(BaseModel):
-    username: str
-    password: str
-    email: str
+    username: str = Field(min_length=6, max_length=15)
+    password: str = Field(min_length=8, max_length=32)
+    email: str = Field(min_length=7)
     role: str
 
 
