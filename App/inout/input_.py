e193dac1d23941fe2ec87367607a70c64ada4092
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-01-20 11:17:29 +0330

    setting validate to user login and user update.

--- App/inout/input_.py
+++ App/inout/input_.py
@@ -4,16 +4,16 @@ from pydantic import BaseModel, Field
 class UserRegisterInput(BaseModel):
     username: str = Field(min_length=6, max_length=15)
     password: str = Field(min_length=8, max_length=32)
-    email: str = Field(min_length=7)
+    email: str = Field(min_length=7, max_length=32)
     role: str
 
 
 class UserLoginInput(BaseModel):
-    username: str
-    password: str
+    username: str = Field(min_length=6, max_length=15)
+    password: str = Field(min_length=8, max_length=32)
 
 
 class UserUpdateInput(BaseModel):
-    username: str
-    email: str
+    username: str = Field(min_length=6, max_length=15)
+    email: str = Field(min_length=7, max_length=32)
     role: str
