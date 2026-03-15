ea98bd91c3157cd659380c068e09d526c8c5bce9
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-03-15 12:43:58 +0330

    removing user/register and user/login, using auth insted.

--- App/routers/users.py
+++ App/routers/users.py
@@ -1,37 +1,14 @@
 from fastapi import APIRouter, status
-from inout.out import OutputGetUserById, CreatedOut
-from inout.input_ import UserRegisterInput, UserLoginInput, UserUpdateInput
+from inout.out import OutputGetUserById
+from inout.input_ import UserUpdateInput, UserLoginInput
 from db.get_db import get_db_session
 from errors import WrongUsernameOrPassword, NotFoundUser, ExistUsername
 from repository.Repository import UserRepository
 from utils.secrets import passwordManager
+from utils.jwt import user_dependency
 
 
 router = APIRouter()
-
-
-@router.post("/register", status_code=status.HTTP_201_CREATED)
-async def register(data: UserRegisterInput, db=get_db_session):
-    if (await UserRepository.is_username(data.username, db)):
-        raise ExistUsername
-
-    data.password = passwordManager.hash(data.password)
-
-    await UserRepository.register_user(data, db)
-
-    user = await UserRepository.get_user_by_username(data.username, db)
-
-    return CreatedOut(id=user.id, username=user.username, role=user.role)
-
-
-@router.post("/login", status_code=status.HTTP_200_OK)
-async def login(data: UserLoginInput, db=get_db_session):
-    user = await UserRepository.get_user_by_username(data.username, db)
-
-    if not user or not (passwordManager.verify(data.password, user.password)):
-        raise WrongUsernameOrPassword
-
-    return {"Login": "Succeed."}
 
 
 @router.delete("/delete", status_code=status.HTTP_204_NO_CONTENT)
