82f8b0f07e139219eba490a6a89cc45e5a8882d7
Cod3Talk3r <mohammadr3za.1@gmail.com>
2026-03-15 12:43:13 +0330

    fixing login token.

--- App/routers/auth.py
+++ App/routers/auth.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter, Depends, status
-from fastapi.security import OAuth2PasswordRequestForm
+# from fastapi.security import OAuth2PasswordRequestForm
 from db.get_db import get_db_session
 from db.models import User
 from errors import ExistUsername, NotFoundUser
 from utils.secrets import passwordManager
 from inout.jwt import JWTResponsePayload
 from utils.jwt import user_authendication, generate_token
-from inout.input_ import UserRegisterInput
+from inout.input_ import UserRegisterInput, UserLoginInput
 from repository.Repository import UserRepository 
 
 
@@ -30,8 +30,9 @@ async def create_user(data: UserRegisterInput, db = get_db_session):
     await UserRepository.register_user(user_model, db)
 
 
+
 @route.post("/token", response_model=JWTResponsePayload)
-async def create_token(data: OAuth2PasswordRequestForm = Depends(), db = get_db_session):
+async def create_token(data: UserLoginInput, db = get_db_session):
     user = await user_authendication(data.username, data.password, db)
 
     if not user:
